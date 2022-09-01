bbd0a783e26c38bda934472f0f652b1df002986e
menuin <menuin@hanyang.ac.kr>
2022-09-01 18:00:57 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import styled, { css, keyframes } from "styled-components";
+import { useState } from "react";
 
+const Container = styled.div`
+  position: relative;
+`;
+const ImgContainer = styled.div`
+  position: absolute;
+`;
+const Button = styled.div`
+  background-color: ${(props) => (props.clicked ? "white" : "black")};
+  position: absolute;
+  left: 50px;
+  top: 50px;
+  height: 100px;
+  width: 100px;
+  display: ${(props) => (props.clicked ? "none" : "block")};
+  &:hover {
+    /* background-color: red; */
+    top: 60px;
+    left: 60px;
+  }
+`;
+const ClickedBtn = styled.div`
+  background-color: white;
+  position: absolute;
+  left: 60px;
+  top: 60px;
+  height: 100px;
+  width: 100px;
+  display: ${(props) => (props.clicked ? "block" : "none")};
+`;
+const Bg = styled.img`
+  /* opacity: 0.5; */
+  opacity: ${(props) => (props.clicked ? "1" : "0")};
+  animation: ${(props) =>
+    props.clicked &&
+    css`
+      ${fadein} 2s;
+    `};
+`;
+
+const fadein = keyframes`
+  0% {
+    opacity:0;
+  }
+  100% {
+    opacity : 1;
+}
+`;
 function App() {
+  const [clicked, setClicked] = useState(false);
+  const onBtnClick = () => {
+    setClicked(!clicked);
+    console.log(clicked);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className="App">
+      <ImgContainer>
+        <Bg clicked={clicked} src="/images/background.jpg"></Bg>
+      </ImgContainer>
+      <Button onClick={onBtnClick} clicked={clicked}></Button>
+      <ClickedBtn clicked={clicked}></ClickedBtn>
+    </Container>
   );
 }
 
