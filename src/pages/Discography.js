d7ed25c5cf8e1288c331bc57f836fa8482c06e13
menuin <menuin@hanyang.ac.kr>
2022-09-26 00:14:27 +0900

    connecting server example

--- src/pages/Discography.js
+++ src/pages/Discography.js
@@ -1,5 +1,40 @@
+import { useState } from "react";
+import styled from "styled-components";
+
+const SampleContainer = styled.div`
+  margin-top: 200px;
+`;
 function Discography() {
-  return <>Discography</>;
+  const [text, setText] = useState("");
+  const handleChange = (e) => {
+    setText(e.target.value);
+  };
+  const onClick = () => {
+    const textbox = {
+      text,
+    };
+    fetch("http://localhost:3001/text", {
+      method: "post",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(textbox),
+    }).then((res) =>
+      res.json().then((json) => {
+        console.log(json);
+        setText(json.text);
+      })
+    );
+  };
+  return (
+    <SampleContainer>
+      <div>
+        <input name="text" onChange={handleChange}></input>
+        <button onClick={onClick}>전송</button>
+        <h3>{text}</h3>
+      </div>
+    </SampleContainer>
+  );
 }
 
 export default Discography;
